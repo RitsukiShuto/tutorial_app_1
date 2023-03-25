f114b4873d02e440146811ebb7be4c334a9819c0
ritsukishuto <rtk410s@gmail.com>
2023-03-25 16:47:07 +0900

    headerに要素を追加

--- lib/main.dart
+++ lib/main.dart
@@ -35,50 +35,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        // body: Text("Hello World"),
-        body: Column(
-          children: [
-            const Text("helloworld"),
-            const Text("happy hacking!"),
-            TextButton(
-              onPressed: () => {print('puressed button!!')},
-              child: const Text("Text Button"),
-            ),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: const [
-                  Icon(
-                    Icons.favorite,
-                    color: Colors.pink,
-                    size: 24.0,
-                  ),
-                  Icon(
-                    Icons.audiotrack,
-                    color: Colors.green,
-                    size: 24.0,
-                  ),
-                  Icon(
-                    Icons.beach_access,
-                    color: Colors.blue,
-                    size: 24.0,
-                  )
-                ]),
-          ],
-        ));
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      // body: Text("Hello World"),
+      body: Column(
+        children: [
+          const Text("helloworld"),
+          const Text("happy hacking!"),
+          TextButton(
+            onPressed: () => {print('puressed button!!')},
+            child: const Text("Text Button"),
+          ),
+          const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(
+                  Icons.favorite,
+                  color: Colors.pink,
+                  size: 24.0,
+                ),
+                Icon(
+                  Icons.audiotrack,
+                  color: Colors.green,
+                  size: 24.0,
+                ),
+                Icon(
+                  Icons.beach_access,
+                  color: Colors.blue,
+                  size: 24.0,
+                )
+              ]),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => {print("pressed???")},
+          child: const Icon(Icons.timer)),
+
+      drawer: const Drawer(child: Center(child: Text("Drawer"))),
+      endDrawer: const Drawer(child: Center(child: Text("EndDrawer"))),
+    );
   }
 }
